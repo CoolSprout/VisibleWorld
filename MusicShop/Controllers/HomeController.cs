42b54578203bfb415875209b563bd0ea9a344548
CoolSprout <js.johni@outlook.com>
2016-09-26 01:01:06 -0400

    Complete with Ajax and jquery $.Post

--- MusicShop/Controllers/HomeController.cs
+++ MusicShop/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using MusicShop.Models;
+﻿using MusicShop.Models;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -16,13 +16,38 @@ namespace MusicShop.Controllers
         [HttpPost]
         public ActionResult Index(string search, string genre, int? fromYear, int? toYear)
         {
-            throw new NotImplementedException();
+            Store StoreData = new Store();
+            List<Album> albumList = (from a in StoreData.Albums
+                                     select a).ToList();
+            if (search != string.Empty)
+            {
+                albumList = albumList.Where(a => a.Title.ToLower().Contains(search.ToLower())).ToList();
+            }
+            if (genre != string.Empty)
+            {
+                albumList = albumList.Where(a => a.Genre.ToLower().Contains(genre.ToLower())).ToList();
+            }
+            if (fromYear != null)
+            {
+                albumList = albumList.Where(a => a.Year >= fromYear).ToList();
+            }
+            if (toYear != null)
+            {
+                albumList = albumList.Where(a => a.Year <= toYear).ToList();
+            }
+            //StoreData.Albums.Clear();
+            //StoreData.Albums.AddRange(albumList);
+            return PartialView("_SearchResults", albumList);
         }
 
         [HttpPost]
         public ActionResult Album(int id)
         {
-            throw new NotImplementedException();
+            Store StoreData = new Store();
+            var albumList = (from a in StoreData.Albums
+                             where a.Id == id
+                             select a).FirstOrDefault();
+            return PartialView("_Album", albumList);
         }
     }
 }
