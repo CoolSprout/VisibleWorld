4ce43c8d3c4783b8c42f7c532cffabe664c98af8
CoolSprout <js.johni@outlook.com>
2016-09-26 21:03:41 -0400

    Sample SQL Cmds

--- MusicShop/Database/Example.sql
+++ MusicShop/Database/Example.sql
@@ -1,1 +1,29 @@
 
+CREATE TABLE [dbo].[Stores]
+(
+	[StoreId] INT NOT NULL PRIMARY KEY, 
+	[Name] NVARCHAR(255) NULL
+)
+
+CREATE TABLE [dbo].[Albums] (
+	[Id]       INT            NOT NULL,
+	[Title]    NVARCHAR (255) NULL,
+	[Artist]   NVARCHAR (255) NULL,
+	[Genre]    NVARCHAR (255) NULL,
+	[Year]     INT            NULL,
+	[CoverURL] NVARCHAR (MAX) NULL,
+	[StoreId] INT NOT NULL, 
+	PRIMARY KEY CLUSTERED ([Id] ASC), 
+	CONSTRAINT [FK_Albums_Stores] FOREIGN KEY ([StoreId]) REFERENCES [Stores]([StoreId])
+);
+
+CREATE TABLE [dbo].[Tracks] (
+	[Number]    INT           NOT NULL,
+	[SongTitle] VARCHAR (255) NULL,
+	[Length]    TIME (0)      NULL,
+	[AlbumID] INT NOT NULL, 
+	PRIMARY KEY CLUSTERED ([Number] ASC), 
+	CONSTRAINT [FK_Tracks_Albums] FOREIGN KEY ([AlbumID]) REFERENCES [Albums]([Id])
+);
+
+
